488552f8f56ad43fcbb54d69373897990c77ff79
camccandler15 <camccandler15@blakeschool.org>
2015-02-09 21:53:09 -0600

    Initial Commit

---
+++ Prototype-Caitlin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Prototype-Caitlin
+//
+//  Created by camccandler15 on 2/9/15.
+//  Copyright (c) 2015 camccandler15. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Prototype-Caitlin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Prototype-Caitlin
+//
+//  Created by camccandler15 on 2/9/15.
+//  Copyright (c) 2015 camccandler15. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
